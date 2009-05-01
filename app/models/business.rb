51478cdcc022f8daeb154092a9f1515c6b79ab35
Sebastien Grosjean <public@zencocoon.com>
2009-05-01 20:00:00 +0300

    Keep unobtrusive geolocalization while taking benefit of front-side advantage and server-side caching system

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,12 +1,19 @@
 class Business < ActiveRecord::Base
+  attr_accessor :current_lat, :current_lng
+  
   acts_as_mappable
   before_validation :geocode_address
 
   private
   def geocode_address
-    return if self.address.nil? || (self.geolocalized_address == self.address && !self.lat.nil? && !self.lng.nil?)
-    geo=Geokit::Geocoders::MultiGeocoder.geocode(address)
-    errors.add(:address, "Could not Geocode address") if !geo.success
-    self.lat, self.lng, self.geolocalized_address = geo.lat, geo.lng, self.address if geo.success
+    if (self.geolocalized_address != self.address)
+      if (self.current_lat == self.lat && self.current_lng == self.lng)
+        geo=Geokit::Geocoders::MultiGeocoder.geocode(address)
+        errors.add(:address, "Could not Geocode address") if !geo.success
+        self.lat, self.lng, self.geolocalized_address = geo.lat, geo.lng, self.address if geo.success
+      else
+        self.geolocalized_address = self.address
+      end
+    end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,30 @@
 Application = Class.create({
   initialize: function() {
-    this.initAddressChooser('business_address', 'business_submit', 'business_lat', 'business_lng');
+    this.initAddressChooser('business', {street: 'address'});
   },
-  initAddressChooser: function(addressId, submitId, latId, lngId) {
-    var address, submit;
-    if (!(address = $(addressId)) || !(submit = $(submitId))) return;
+  initAddressChooser: function(object, options) {
+    // Init options
+    options = $H({
+      street: 'street',
+      submit: 'submit',
+      lat: 'lat',
+      lng: 'lng',
+      current_lat: 'current_lat',
+      current_lng: 'current_lng',
+      suggests: 'suggests'
+    }).merge(options);
+    
+    // Check if current lat/lng are defined to use them
+    var current_lat, current_lng;
+    if ((current_lat = $(object+'_'+options.get('current_lat'))) && (current_lng = $(object+'_'+options.get('current_lng'))))
+      current_lng.insert({after: '<input type="hidden" id="'+object+'_'+options.get('lat')+'" name="'+object+'['+options.get('lat')+']" value="'+current_lat.value+'" /><input type="hidden" id="'+object+'_'+options.get('lng')+'" name="'+object+'['+options.get('lng')+']" value="'+current_lng.value+'" />'});
+    
+    var street, submit;
+    if (!(street = $(object+'_'+options.get('street'))) || !(submit = $(object+'_'+options.get('submit')))) return;
     
     // BEGIN AUTOCOMPLETE SETTINGS AND HACKS :)
     // Create a local autocomplete without data. Data will be added dynamically according to map suggestions
-    var autocomplete = new Autocompleter.Local(addressId, 'suggests', [],
+    var autocomplete = new Autocompleter.Local(street, options.get('suggests'), [],
       {
         afterUpdateElement: function(element, selectedElement) {
           var index = selectedElement.up().immediateDescendants().indexOf(selectedElement);
@@ -30,26 +46,23 @@ Application = Class.create({
       widget.showPlacemark(this.index);
     });
     // END AUTOCOMPLETE SETTINGS AND HACKS :)
-
-
+    
     widget = new Maptimize.AddressChooser.Widget(
       { onInitialized: function(widget) {
           // Add default controls
           widget.getMap().setUIToDefault();
-
+    
           widget.initMap();
-
+    
           // Observe 'suggests:started' to display spinner and disable submit button
           widget.addEventListener('suggests:started', function() {
-            address.addClassName('spinner');
-            submit.disabled = true;
+            street.addClassName('spinner');
           });
           
           // Observe 'suggests:found' to hide spinner and enable submit button if a placemark has been found
           widget.addEventListener('suggests:found', function(placemarks) {
-            submit.disabled = false;
-            address.removeClassName('spinner');
-            address.focus();
+            street.removeClassName('spinner');
+            street.focus();
           
             // Reset autocomplete suggestions to new placemarks
             autocomplete.options.array.clear();
@@ -66,12 +79,11 @@ Application = Class.create({
             }
           });
           
-          submit.disabled = true;
-          address.focus();
+          street.focus();
         },
-        street: addressId,
-        lat: latId,
-        lng: lngId
+        street: object+'_'+options.get('street'),
+        lat: object+'_'+options.get('lat'),
+        lng: object+'_'+options.get('lng')
       }
     );
 
